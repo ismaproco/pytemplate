f9ff2103b03ebe975986feea22e41d3c74f8f420
ijimenezgarzon <ijimenezgarzon@gmail.com>
2013-12-19 10:40:57 -0600

    Create template.tf

---
+++ template.tf
@@ -0,0 +1,17 @@
+[imgs]
+[css{style.css}]
+[templates[projects{list.html,edit.html},users{list.html,edit.html}]]
+[js
+	[libs
+		[
+		jquery{jquery.min.js},backbone{backbone.min.js},underscore{underscore.min.js}],
+	 models{users.js,projects.js},
+	 collections{users.js,projects.js},
+	 views
+		[
+		projects{list.js,edit.js},
+		users{list.js,edit.js},
+		]
+	]{router.js, app.js, main.js, order.js, text.js}
+]
+{index.html}
